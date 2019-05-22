40594fa1ddc11f27aecbb47624deb27be96f2d8f
Yurii Salashnyi <yurik.salashnuy@gmail.com>
2019-05-22 11:43:54 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Video/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Video from './components/Video'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Video,
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Video.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Video',
+}
+</script>
+
+<style scoped>
+
+</style>
