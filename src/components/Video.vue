1f91871745ccbe6fb2a86379d5d466edd6d06d63
Yurii Salashnyi <yurik.salashnuy@gmail.com>
2019-05-22 13:15:32 +0300

    video

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-    test
+  <div class="video-wrap">
+    <video ref="video" autoplay muted></video>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Video',
-}
+  import DoxyMe from '@doxyme/media-access'
+
+  export default {
+    name: 'Video',
+    mounted() {
+      this.requestMediaAccess()
+    },
+    methods: {
+      async requestMediaAccess() {
+        try {
+          this.$refs.video.srcObject = await DoxyMe.requestMediaAccess()
+        } catch (e) {
+
+        }
+      },
+    },
+  }
 </script>
 
 <style scoped>
+  .video-wrap {
+    display: flex;
+    justify-content: center;
+    margin: 30px 0;
+  }
 
+  video {
+    background-color: #ccc;
+  }
 </style>
